0457c5a0fb11b63a5a30c520dbb0d68875704dbb
rahul <rkrahul8181@gmail.com>
2024-03-08 21:32:12 +0530

    [frontend] add socket.io-client

---
+++ frontend/src/components/provider/socket-provider.tsx
@@ -0,0 +1,36 @@
+
+import React, {createContext,useContext, useEffect, useState } from "react";
+
+import {io,Socket} from "socket.io-client"
+interface IsocketContext{
+    socket:null | Socket,
+    isConnected:boolean
+}
+const SocketContext = createContext<IsocketContext>({
+    socket:null,
+    isConnected:false
+
+});
+
+export const useSocket = ()=>{
+    return useContext(SocketContext)
+}
+
+const SocketProvider = ({children}:{children:React.ReactNode}) => {
+    const [socket, setSocket] = useState<Socket | null>(null);
+    const [isConnected, setIsConnected] = useState(false);
+
+    useEffect(() => {
+        const socket = io(import.meta.env.VITE_APP_SOCKET_URL!);
+        setSocket(socket);
+        setIsConnected(true);
+    }, []);
+
+    return (
+        <SocketContext.Provider value={{ socket, isConnected }}>
+            {children}
+        </SocketContext.Provider>
+    );
+};
+
+export default SocketProvider;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './router'
-import { ThemeProvider } from './components/provider/theme-provider'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import { ThemeProvider } from "./components/provider/theme-provider";
+import SocketProvider from "./components/provider/socket-provider";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="chat-loom-theme" >
-
-    <RouterProvider router={router} />
-    </ThemeProvider>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <SocketProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="chat-loom-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </SocketProvider>
+);
