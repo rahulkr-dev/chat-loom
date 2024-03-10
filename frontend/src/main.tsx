7ceba80e24f46a739d08817e1d2acaa4e89201a7
rahul <rkrahul8181@gmail.com>
2024-03-10 09:37:36 +0530

    [frontend] add react-query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,14 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import { ThemeProvider } from "./components/provider/theme-provider";
 import SocketProvider from "./components/provider/socket-provider";
+import { QueryProvider } from "./components/provider/query-provider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
+  <QueryProvider>
     <SocketProvider>
       <ThemeProvider defaultTheme="dark" storageKey="chat-loom-theme">
         <RouterProvider router={router} />
       </ThemeProvider>
     </SocketProvider>
+  </QueryProvider>
 );
