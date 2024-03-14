f2e14337bc5230bd0f349f95d21ce0bcaa327d31
rahul <rkrahul8181@gmail.com>
2024-03-14 23:21:52 +0530

    [backend] update file structure for storing constant

--- backend/src/common/constants/index.ts
+++ backend/src/common/constants/index.ts
@@ -1,4 +1,6 @@
-export const userRoles = {
-    ADMIN: "admin",
-    USER: "user",
+export const ERROR_MESSAGES: Readonly<Record<string, string>> = {
+    usernameExists: "Username is already taken. Please choose another.",
+    emailExists: "Email is already registered. Please use a different one.",
+    invalidCredentials: "Invalid username or password. Please try again.",
+    unauthorized: "Unauthorized access. Ensure you have proper permissions.",
 };
