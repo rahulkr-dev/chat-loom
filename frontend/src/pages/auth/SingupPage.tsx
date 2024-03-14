afc4c9a11cdedc2b9400323daf354c6ff601ecb6
rahul <rkrahul8181@gmail.com>
2024-03-14 18:33:07 +0530

    [frontend] refactor style signup page

--- frontend/src/pages/auth/SingupPage.tsx
+++ frontend/src/pages/auth/SingupPage.tsx
@@ -1,13 +1,6 @@
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { CardWrapper } from "@/components/card-wrapper";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+
 import {
   Form,
   FormControl,
@@ -23,37 +16,25 @@ import { Tsignup } from "@/types";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 import { z } from "zod";
 
-const ErrorMessage = ({ error }: { error: string }) => {
-  return (
-    <Alert variant="destructive">
-      {/* <ExclamationTriangleIcon className="h-4 w-4" /> */}
-      <AlertTitle>Error</AlertTitle>
-      <AlertDescription>
-        {error}
-      </AlertDescription>
-    </Alert>
-  );
+const signup = async (payload: Tsignup) => {
+  const { data } = await singupMtFn(payload);
+  return data;
 };
 
-const signup = async(payload:Tsignup)=>{
-  const {data} = await singupMtFn(payload)
-  return data
-}
-
 const SignupPage = () => {
-  const queryClient = useQueryClient()
-  const { mutate } = useMutation({
+  const queryClient = useQueryClient();
+  const { mutate, isPending } = useMutation({
     mutationFn: signup,
-    onSuccess:()=>{
-      queryClient.invalidateQueries({queryKey:["auth/self"]})
-
-    }
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["auth/self"] });
+    },
   });
+
   // todo - server error message
-  
- 
+
   const form = useForm<z.infer<typeof singupSchema>>({
     resolver: zodResolver(singupSchema),
     defaultValues: {
@@ -68,81 +49,82 @@ const SignupPage = () => {
     mutate(values);
   };
   return (
-    <div className="w-screen h-screen flex justify-center items-center ">
-      <Card className="lg:w-[50vw]">
-        <CardHeader>
-          <CardTitle>Signup</CardTitle>
-          <CardDescription>
-            Create a new account to access all the features of our application.
-            Fill in the required fields below to get started.
-          </CardDescription>
-        </CardHeader>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
-            <CardContent>
-              <FormField
-                name="username"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                name="name"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Name</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                name="email"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormItem>Email</FormItem>
-                    <FormControl>
-                      <Input type="email" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                name="password"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input type="password" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <ErrorMessage error="something wentwrong" />
-            </CardContent>
-            <CardFooter>
-              <Button type="submit" className="w-full ">
-                Signup
-              </Button>
-            </CardFooter>
-          </form>
-        </Form>
-      </Card>
-    </div>
+    <CardWrapper
+    headerLabel="Welcome back"
+    backButtonLabel="Have an account?"
+    backButtonHref="/login"
+  >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div>
+            <FormField
+              name="username"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="name"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="email"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormItem>Email</FormItem>
+                  <FormControl>
+                    <Input type="email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+                 <Button
+                    size="sm"
+                    variant="link"
+                    asChild
+                    className="px-0 font-normal"
+                  >
+                    <Link to="/reset">Forgot password?</Link>
+                  </Button>
+          </div>
+
+          <Button disabled={isPending} type="submit" className="w-full">
+            Signup
+          </Button>
+        </form>
+      </Form>
+</CardWrapper>
   );
 };
 
